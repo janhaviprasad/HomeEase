abf1104570f9223cf27a3083fccfd340556bbcb3
janhaviprasad <166502005+janhaviprasad@users.noreply.github.com>
2026-08-11 09:32:51 +0530

    Delete Express Backend directory

--- Express Backend/booking-service/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router=express.Router();
-const verifyTOken=require("../middleware/authMiddleware")
-const {createReview}=require("../controllers/reviewController")
-
-router.post("/",verifyTOken,createReview);
-
-module.exports=router;
